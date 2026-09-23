0e67b85fa38a7da291c0967dbb4f0500b03c130d
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-09-23 19:51:04 +0100

    git push origin mainRevert "restored sitemap-planner files from stale branch"
    This reverts commit d18b3c0230a2d07fee2767d4e140000885e079b9.

--- sitemap-planner/src/access/index.ts
+++
@@ -1,6 +0,0 @@
-import {setupJsonBodyParse} from "./jsonParser.js";
-import {Application} from "express";
-
-export default (app: Application) => {
-    setupJsonBodyParse(app)
-}
--- sitemap-planner/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import {Application} from "express";
-import {setupStatusRoutes} from "./status-router";
-import {setupSitemapRoutes} from './sitemap-router';
-
-export default (app: Application) => {
-    setupStatusRoutes(app)
-    setupSitemapRoutes(app)
-}
